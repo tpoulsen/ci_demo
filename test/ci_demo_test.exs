888086f678e0511b8f7bcedb0c254a03c31e6f4f
Travis Poulsen <travis.poulsen@gmail.com>
2017-04-26 10:05:26 -0400

    Add a test and config for Travis CI
    Adds a property test for addition, and minimal configuration to run on
    Travis CI.

--- test/ci_demo_test.exs
+++ test/ci_demo_test.exs
@@ -1,8 +1,15 @@
 defmodule CiDemoTest do
   use ExUnit.Case
+  use ExCheck
   doctest CiDemo
 
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  property "addition is commutative" do
+    for_all {x, y} in  {int(), int()} do
+      assert x + y == y + x
+    end
   end
 end
